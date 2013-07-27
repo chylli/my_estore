59fdefcdc6c09cd9d4d36ef90a35f705f571de34
Chylli <chylli.email@gmail.com>
2013-07-27 19:21:53 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyEstore::Application.config.secret_key_base = '3a5896509247fd569088eb561efd3874403271be07d4707cf339cf2740e100ca533763badb3ac88b7406a682e77b4a6520ec08d51ed0bfe9a974ebc297d2b4b8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyEstore::Application.config.session_store :cookie_store, key: '_my_estore_session'
